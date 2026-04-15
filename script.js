bfab91e8c4e9b7b5ff5a9d3634ea38900dc913ac
Eduardo Soria <262416161+edusoria-dev@users.noreply.github.com>
2026-04-15 23:23:43 +0200

    Mejoras 15 abril 23:23

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ window.addEventListener('scroll', () => {
         skillsSection.classList.add('active');
     }
 });
+
+const items = document.querySelectorAll('.timeline-item');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+items.forEach(item => {
+    observer.observe(item);
+});
